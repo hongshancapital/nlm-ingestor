2e21018de410366315cabe9efc9a074c8f3c2b5f
金海岩 <yhjin.codedance@hongshan.com>
2024-04-08 11:54:49 +0800

    接收 S3 URL 参数，从 S3 下载文件到本地

--- nlm_ingestor/ingestion_daemon/__main__.py
+++ nlm_ingestor/ingestion_daemon/__main__.py
@@ -7,12 +7,15 @@ from flask import Flask, request, jsonify, make_response
 from werkzeug.utils import secure_filename
 from nlm_ingestor.ingestor import ingestor_api
 from nlm_utils.utils import file_utils
+import boto3
 
 app = Flask(__name__)
 
 # initialize logging
 logger = logging.getLogger(__name__)
 logger.setLevel(cfg.log_level())
+
+s3_client = boto3.client('s3')
 
 @app.route('/', methods=['GET'])
 def health_check():
@@ -26,8 +29,8 @@ def parse_document(
     render_format = request.args.get('renderFormat', 'all')
     use_new_indent_parser = request.args.get('useNewIndentParser', 'no')
     apply_ocr = request.args.get('applyOcr', 'no')
-    file = request.files['file']
-    original_filename = file.filename
+    original_filename = None
+    filename = None
     tmp_file = None
     try:
         parse_options = {
@@ -38,13 +41,25 @@ def parse_document(
             "apply_ocr": apply_ocr == "yes"
         }
         # save the incoming file to a temporary location
-        filename = secure_filename(file.filename)
-        _, file_extension = os.path.splitext(file.filename)
-        tempfile_handler, tmp_file = tempfile.mkstemp(suffix=file_extension)
-        os.close(tempfile_handler)
-        file.save(tmp_file)
+        if 'file' in request.files:
+            file = request.files['file']
+            original_filename = file.filename
+            filename = secure_filename(original_filename)
+            tmp_file = create_temp_file(filename)
+            file.save(tmp_file)
+        elif 'filename' in request.form and 's3url' in request.form:
+            original_filename = request.form.get('filename')
+            filename = secure_filename(original_filename)
+            tmp_file = create_temp_file(filename)
+            s3url = request.form.get('s3url')
+            bucket_name, file_key = parse_s3_url(s3url)
+            s3_client.download_file(bucket_name, file_key, tmp_file)
+        else:
+            raise Exception("No file found in request")
+
         # calculate the file properties
         props = file_utils.extract_file_properties(tmp_file)
+        print(f"Parsing document: '{original_filename}'")
         logger.info(f"Parsing document: '{original_filename}'")
         return_dict, _ = ingestor_api.ingest_document(
             filename,
@@ -70,6 +85,28 @@ def parse_document(
         if tmp_file and os.path.exists(tmp_file):
             os.unlink(tmp_file)
     return make_response(jsonify({"status": status, "reason": msg}), rc)
+
+
+def create_temp_file(filename):
+    """
+    Create a temporary file with the same extension as the input filename.
+    """
+    _, file_extension = os.path.splitext(filename)
+    tempfile_handler, temp_file = tempfile.mkstemp(suffix=file_extension)
+    os.close(tempfile_handler)
+    return temp_file
+
+
+def parse_s3_url(s3url):
+    """
+    Parse the S3 URL to get the bucket name and file key.
+    e.g. s3://somebucket/path/to/file.pdf -> ('somebucket', 'path/to/file.pdf')
+    """
+    parts = s3url.split('/')
+    bucket_name = parts[2]
+    file_key = '/'.join(parts[3:])
+    return bucket_name, file_key
+
 
 def main():
     logger.info("Starting ingestor service..")
